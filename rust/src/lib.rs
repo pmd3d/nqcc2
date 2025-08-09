aaaa4ce23861394c1ac77a30c1233bb1435e1367
Jonathan Lim <pmd3d@users.noreply.github.com>
2025-08-09 00:11:05 -0700

    Add type table module in Rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,8 @@
-pub mod tokens;
-pub mod consts;
-pub mod types;
-pub mod token_stream;
-pub mod lex;
 pub mod ast;
+pub mod consts;
+pub mod lex;
 pub mod parse;
+pub mod token_stream;
+pub mod tokens;
+pub mod type_table;
+pub mod types;
